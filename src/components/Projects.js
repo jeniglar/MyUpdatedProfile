8e970d66d51f687bbf7e8f9a345cdda932899a21
JenIglar <jeniglar@gmail.com>
2020-11-08 22:24:35 -0500

    Card styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,12 +5,12 @@ import Card from 'react-bootstrap/Card';
 function CreateProject(props) {
     return (
         <>
-            <Card className="mt-2 mr-3 border-dark">
+            <Card className="card mt-2 mr-3 border-dark">
                 <Card.Img src={props.image} className="card-img-top img-fluid" alt={props.project} />
                 <Card.Body>
-                    <Card.Title className="cardTitle"> {props.project} </Card.Title>
-                    <a href={props.link} target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> Deployed Application</a>
-                    <a href={props.repository} target="_blank" rel="noreferrer" className="btn mb-1 btn-primary"> GitHub Repository </a>
+                    <Card.Title className="project"> {props.project} </Card.Title>
+                    <a href={props.link} target="_blank" rel="noreferrer" className="btn mb-1"> Deployed Application</a>
+                    <a href={props.repository} target="_blank" rel="noreferrer" className="btn mb-1"> GitHub Repository </a>
                 </Card.Body>
             </Card>
         </>
